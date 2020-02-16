c8531a70f20663b3cc69012d15d52276f07b63c2
cheezenaan <cheezenaan@gmail.com>
2020-02-16 23:27:02 +0900

    abc(155c): Use Array#each_with_object
    ref. https://atcoder.jp/contests/abc155/submissions/10164902

--- atcoder/abc/155/c.rb
+++ atcoder/abc/155/c.rb
@@ -4,8 +4,7 @@ N = gets.to_i
 S = N.times.map { gets.chomp }
 
 # 要素の個数を集計する Hash を別に用意
-h = Hash.new(0)
-S.each { |s| h[s] += 1 }
-max = h.values.max
+counts = S.each_with_object(Hash.new(0)) { |s, h| h[s] += 1 }
+max = counts.values.max
 
-puts h.select { |_k, v| v == max }.keys.sort
+puts counts.select { |_k, v| v == max }.keys.sort
