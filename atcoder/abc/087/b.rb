0f50c00abc315c8eb637136063b3b6fa3cd4decf
cheezenaan <cheezenaan@gmail.com>
2020-02-17 09:52:37 +0900

    abc(087b): Use Integer#upto instead of Range#each

--- atcoder/abc/087/b.rb
+++ atcoder/abc/087/b.rb
@@ -3,8 +3,8 @@
 a, b, c, x = 4.times.map { gets.to_i }
 
 ans = 0
-(0..a).each do |i|
-  (0..b).each do |j|
+0.upto(a) do |i|
+  0.upto(b) do |j|
     sum = 500 * i + 100 * j
     rest = x - sum
     ans += 1 if rest >= 0 && rest <= c * 50
