812b8371eed0e149ac7afb3ef05432d5a2aad065
cheezenaan <cheezenaan@gmail.com>
2020-02-23 15:36:20 +0900

    abc: Solve abc104b

---
+++ atcoder/abc104/b.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+S = gets.chomp
+
+ans = 'AC'
+
+ans = 'WA' unless S.start_with?('A')
+ans = 'WA' if S[2..-2].count('C') != 1
+ans = 'WA' if S.count('A-Z') > 2
+
+puts ans
