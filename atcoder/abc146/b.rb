50dba916f67bd3376b447125ef9a6bf659c9dfc9
cheezenaan <cheezenaan@gmail.com>
2020-02-24 11:35:54 +0900

    abc(146b): Use Integer#times instead of while loop

--- atcoder/abc146/b.rb
+++ atcoder/abc146/b.rb
@@ -3,14 +3,12 @@
 n = gets.to_i
 s = gets.chomp
 
-count = 0
 ans = s.chars
 
-while count < n
+n.times do
   0.upto(s.size - 1) do |i|
     ans[i] = ans[i].next[-1]
   end
-  count += 1
 end
 
 print ans.join('')
