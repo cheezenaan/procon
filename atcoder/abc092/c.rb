3212185bd35cab8e02daa89d12bdc344b7abcc76
cheezenaan <cheezenaan@gmail.com>
2020-02-25 01:17:07 +0900

    abc(092c): Remove the redundant array of costs
    and use Enumerable#inject to calculate the total of costs

--- atcoder/abc092/c.rb
+++ atcoder/abc092/c.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 n = gets.to_i
-a = [0, *gets.split.map(&:to_i), 0] # a[0] = a[n] = 0 とおいてよいはず
+a = [0, *gets.split.map(&:to_i), 0]
 
-costs = Array.new(a.size, 0)
-1.upto(a.size - 1) do |i|
-  costs[i] = (a[i - 1] - a[i]).abs
+sum = (0..n).inject(0) do |r, i|
+  r + (a[i + 1] - a[i]).abs
 end
-sum = costs.inject(:+)
 
 (1..n).each do |i|
-  d1 = costs[i..i + 1]
+  d1 = (a[i] - a[i - 1]).abs + (a[i + 1] - a[i]).abs
   d2 = (a[i + 1] - a[i - 1]).abs
 
-  puts sum - d1.inject(:+) + d2
+  puts sum - d1 + d2
 end
