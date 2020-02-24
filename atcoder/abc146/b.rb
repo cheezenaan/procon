102cd4d26fb50449f7346bfa8aecb7b1f2596468
cheezenaan <cheezenaan@gmail.com>
2020-02-24 11:35:18 +0900

    abc: Solve abc146b

---
+++ atcoder/abc146/b.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+n = gets.to_i
+s = gets.chomp
+
+count = 0
+ans = s.chars
+
+while count < n
+  0.upto(s.size - 1) do |i|
+    ans[i] = ans[i].next[-1]
+  end
+  count += 1
+end
+
+print ans.join('')
