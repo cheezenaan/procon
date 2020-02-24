99c0150233d94cb3b7bfac06635eef6d2e42f9d0
cheezenaan <cheezenaan@gmail.com>
2020-02-25 08:57:21 +0900

    abc: Solve abc141b

---
+++ atcoder/abc141/b.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+s = gets.chomp
+
+ans = 'Yes'
+RUD = %w[R U D].freeze
+LUD = %w[L U D].freeze
+
+s.each_char.with_index do |v, i|
+  next if i.even? && RUD.include?(v)
+  next if i.odd? && LUD.include?(v)
+
+  ans = 'No'
+end
+puts ans
