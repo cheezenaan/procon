13ea1893f69547b415274d9d72ef401a0cc50206
cheezenaan <cheezenaan@gmail.com>
2020-02-26 09:24:25 +0900

    abc: Solve abc141c

---
+++ atcoder/abc141/c.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+n, k, q = gets.split.map(&:to_i)
+a = q.times.map { gets.to_i }
+points = Array.new(n, k - q)
+
+q.times do |i|
+  points[a[i] - 1] += 1
+end
+
+points.each { |p| puts p > 0 ? 'Yes' : 'No' }
