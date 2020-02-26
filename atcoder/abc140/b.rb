77460b1c1892751a65a7c6f7de90778adf809a8c
cheezenaan <cheezenaan@gmail.com>
2020-02-26 09:31:29 +0900

    abc(140b): Begin loop with zero

--- atcoder/abc140/b.rb
+++ atcoder/abc140/b.rb
@@ -6,14 +6,9 @@ a = gets.split.map(&:to_i)
 b = gets.split.map(&:to_i)
 c = gets.split.map(&:to_i)
 
-ans = b[a[0] - 1]
-
-ans += (1...n).inject(0) do |r, i|
-  if a[i] == a[i - 1] + 1
-    r + b[a[i] - 1] + c[a[i] - 1 - 1]
-  else
-    r + b[a[i] - 1]
-  end
+ans = (0...n).inject(0) do |r, i|
+  extra = a[i + 1] == a[i] + 1 ? c[a[i] - 1] : 0
+  r + b[a[i] - 1] + extra
 end
 
 puts ans
