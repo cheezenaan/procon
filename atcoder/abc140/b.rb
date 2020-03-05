443cff0651fe51cdc66f1addad745c9402f0c4e4
cheezenaan <cheezenaan@gmail.com>
2020-03-05 09:46:59 +0900

    abc(140b): Use Integer#upto instead of Range object

--- atcoder/abc140/b.rb
+++ atcoder/abc140/b.rb
@@ -6,7 +6,7 @@ a = gets.split.map(&:to_i)
 b = gets.split.map(&:to_i)
 c = gets.split.map(&:to_i)
 
-ans = (0...n).inject(0) do |r, i|
+ans = 0.upto(n - 1).inject(0) do |r, i|
   extra = a[i + 1] == a[i] + 1 ? c[a[i] - 1] : 0
   r + b[a[i] - 1] + extra
 end
