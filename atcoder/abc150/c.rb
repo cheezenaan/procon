f763660d27f1f06cbbe8335b84767058de98f8c9
cheezenaan <cheezenaan@gmail.com>
2020-03-06 23:07:33 +0900

    abc(150c): Remove redundant argument in Array#join

--- atcoder/abc150/c.rb
+++ atcoder/abc150/c.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 n = gets.to_i
-a = [*1..n].permutation.map { |n| n.join('') }
+a = [*1..n].permutation.map(&:join)
 
-p = gets.split.map(&:to_i).join('')
-q = gets.split.map(&:to_i).join('')
+p = gets.split.map(&:to_i).join
+q = gets.split.map(&:to_i).join
 
 puts (a.find_index(p) - a.find_index(q)).abs
