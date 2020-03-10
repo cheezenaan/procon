4ab71a482cf8bd3359d1696ddbdd37879b665af7
cheezenaan <cheezenaan@gmail.com>
2020-03-10 09:14:08 +0900

    abc: Solve abc142c

---
+++ atcoder/abc142/c.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+n = gets.to_i
+a = gets.split.map(&:to_i)
+
+h = Hash.new(0)
+n.times.map do |i|
+  h[i + 1] = a[i]
+end
+
+puts h.sort_by(&:last).to_h.keys.join(' ')
