ed4505f33ce2866e7721483c7dc37f0e8a4dcbd6
cheezenaan <cheezenaan@gmail.com>
2020-03-10 21:27:51 +0900

    abc: Solve abc139c

---
+++ atcoder/abc139/c.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+n = gets.to_i
+h = gets.split.map(&:to_i)
+
+max = 0
+cur = 0
+(n - 1).times do |i|
+  if h[i] >= h[i + 1]
+    cur += 1
+  else
+    cur = 0
+  end
+  max = max > cur ? max : cur
+end
+
+puts max
