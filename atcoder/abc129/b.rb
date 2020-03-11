fefddeae7045ffd067a5b16c428aaa1c39aeef6d
cheezenaan <cheezenaan@gmail.com>
2020-03-11 09:01:02 +0900

    abc(129b): Aboid double-loop

--- atcoder/abc129/b.rb
+++ atcoder/abc129/b.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
 n = gets.to_i
-w = gets.split.map(&:to_i)
+ws = gets.split.map(&:to_i)
 
-ans = (w[0] - w[1..-1].inject(:+)).abs
+overall = ws.inject(:+)
+ans = overall
 
-(n - 1).times do |i|
-  s1 = w[0..i].inject(:+)
-  s2 = w[(i + 1)..-1].inject(:+)
-
+s1 = 0
+ws.each do |w|
+  s1 += w
+  s2 = overall - s1
   abs = (s1 - s2).abs
+
   ans = abs if abs <= ans
 end
 
