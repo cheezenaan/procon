1eca240a049213ce1c4bd8eadc3bcd5caacdd16a
cheezenaan <cheezenaan@gmail.com>
2020-03-12 22:49:16 +0900

    abc: Solve abc144c
    Use Prime.prime_division to enumerate divisors
    
    ref. https://qiita.com/norioc/items/5c270ad6ff5d22d1efd0#comment-da13f6b749dba662ec63

---
+++ atcoder/abc144/c.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'prime'
+
+def divisors(num)
+  num.prime_division.inject([1]) do |arr, (p, e)|
+    (0..e).map { |e1| p**e1 }.product(arr).map { |a, b| a * b }
+  end.sort
+end
+
+n = gets.to_i
+d = divisors(n)
+
+ans = (n - 1)
+((d.length + 1) / 2).times do |i|
+  xdiff = (d[i] - 1)
+  ydiff = (d[-1 * (i + 1)] - 1)
+
+  ans = xdiff + ydiff if xdiff + ydiff <= ans
+end
+
+puts ans
