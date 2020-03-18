b73d42fa3521d641a0f09415f966e9c83e35a0f0
cheezenaan <cheezenaan@gmail.com>
2020-03-18 09:11:32 +0900

    abc: Solve abc121b

---
+++ atcoder/abc121/b.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+n, m, c = gets.split.map(&:to_i)
+bs = gets.split.map(&:to_i)
+
+ans = 0
+
+n.times do
+  sum = 0
+  as = gets.split.map(&:to_i)
+  m.times do |j|
+    sum += as[j] * bs[j]
+  end
+
+  ans += 1 if sum + c > 0
+end
+
+puts ans
