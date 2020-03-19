ce9af61c30322a6aebfe6d6b417b9e7eb70650ae
cheezenaan <cheezenaan@gmail.com>
2020-03-19 09:28:13 +0900

    abc(120b): Use Enumerable#select
    instead of Enumrable#each_with_object

--- atcoder/abc120/b.rb
+++ atcoder/abc120/b.rb
@@ -2,8 +2,8 @@
 
 a, b, k = gets.split.map(&:to_i)
 
-divisors = 1.upto([a, b].min).each_with_object([]) do |n, arr|
-  arr.unshift(n) if a % n == 0 && b % n == 0
+divisors = 1.upto([a, b].min).select do |n|
+  a % n == 0 && b % n == 0
 end
 
-puts divisors[k - 1]
+puts divisors[-k]
