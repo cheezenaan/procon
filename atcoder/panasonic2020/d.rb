fcc25088d7da700c6af05bfa783df240a5c3682b
cheezenaan <cheezenaan@gmail.com>
2020-03-22 18:49:54 +0900

    Solve panasonic2020d
    with reading editorial

---
+++ atcoder/panasonic2020/d.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal:false
+
+def dfs(str, max, depth)
+  return puts str if str.length == depth
+
+  'a'.upto(max) do |c|
+    dfs(str + c, (c == max ? max.succ : max), depth)
+  end
+end
+
+n = gets.to_i
+dfs('', 'a', n)
