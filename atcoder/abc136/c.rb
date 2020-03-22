71e3bd4da92693ca6633c57a7374f66627952995
cheezenaan <cheezenaan@gmail.com>
2020-03-22 10:21:53 +0900

    abc: Solve abc136c

---
+++ atcoder/abc136/c.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+n = gets.to_i
+hs = gets.split.map(&:to_i)
+
+ans = 'Yes'
+last = hs[0]
+
+hs.each do |h|
+  if last <= h
+    last = h
+    next
+  end
+
+  # last > h
+  if last == h + 1
+    last = h + 1
+    next
+  end
+
+  ans = 'No'
+  break
+end
+
+puts ans
