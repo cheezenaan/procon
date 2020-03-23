85f7e5804f2cf325339e050255faf233ac64ef27
cheezenaan <cheezenaan@gmail.com>
2020-03-23 09:15:30 +0900

    abc: Solve abc118b

---
+++ atcoder/abc118/b.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+n, m = gets.split.map(&:to_i)
+_, *likes = gets.split.map(&:to_i)
+
+(n - 1).times do
+  _, *a = gets.split.map(&:to_i)
+
+  likes &= a
+end
+
+puts likes.count
