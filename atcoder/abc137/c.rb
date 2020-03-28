be62e6286bcedc8612b1528c0f89fd56d7bdbf3a
cheezenaan <cheezenaan@gmail.com>
2020-03-28 20:39:32 +0900

    abc(137c): Extract strings in each loop

--- atcoder/abc137/c.rb
+++ atcoder/abc137/c.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 n = gets.to_i
-ss = n.times.map { gets.chomp }
 
-ans = 0
 counts = Hash.new(-1)
+ans = 0
 
-ss.each do |s|
-  s = s.chars.sort!.join
+n.times do
+  s = gets.chomp.chars.sort!.join
+
   counts[s] += 1
   ans += counts[s]
 end
