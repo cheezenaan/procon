e07f646245963d6dc62aa931cc121f68a04b30c9
cheezenaan <cheezenaan@gmail.com>
2020-04-02 09:08:24 +0900

    abc: Solve abc107b

---
+++ atcoder/abc107/b.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+h, w = gets.split.map(&:to_i)
+ahw = h.times.map { gets.chomp.split('') }
+
+ahw.select { |ah| ah.include?('#') }
+   .transpose
+   .select { |aw| aw.include?('#') }
+   .transpose.map(&:join)
+   .each { |ah| puts ah }
