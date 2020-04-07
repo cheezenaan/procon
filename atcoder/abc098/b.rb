b6c84c7a174dc2c614b65fdf38d50461d907595c
cheezenaan <cheezenaan@gmail.com>
2020-04-07 09:15:31 +0900

    abc: Solve abc098b

---
+++ atcoder/abc098/b.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+n = gets.to_i
+s = gets.chomp
+
+ans = 0
+
+(n - 1).times do |i|
+  l = s.chars[0..i]
+  r = s.chars[i + 1..-1]
+  i = (l & r).uniq
+
+  ans = i.size if i.size >= ans
+end
+
+puts ans
