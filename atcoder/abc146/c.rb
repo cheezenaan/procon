30961b8b9c03fc0ef91941be6de966f2fb585918
cheezenaan <cheezenaan@gmail.com>
2020-04-11 17:38:35 +0900

    abc: Solve abc146 with editorial-pdf-assited

---
+++ atcoder/abc146/c.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+def buyable?(n, a, b, x)
+  a * n + b * n.to_s.length <= x
+end
+
+a, b, x = gets.split.map(&:to_i)
+n_max = 10**9
+
+if buyable?(n_max, a, b, x)
+  puts n_max
+else
+  puts (1..x).bsearch { |n| !buyable?(n, a, b, x) } - 1
+end
