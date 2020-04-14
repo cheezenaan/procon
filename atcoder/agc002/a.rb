29cd56a78fa02d72dbe7ed4d6f9fdef5fc49b380
cheezenaan <cheezenaan@gmail.com>
2020-04-14 09:18:06 +0900

    Solve agc002_a

--- atcoder/agc002/a.rb
+++ atcoder/agc002/a.rb
@@ -1,9 +1,9 @@
 a, b = gets.split.map(&:to_i)
 
-if a > 0
+if (a * b) <= 0
+  puts "Zero"
+elsif a > 0
   puts "Positive"
 elsif b < 0
-  puts (b - a + 1) % 2 == 0 ? "Positive" : "Negative"
-elsif (a * b) <= 0
-  puts "Zero"
+  puts (b - a).odd? ? "Positive" : "Negative"
 end
